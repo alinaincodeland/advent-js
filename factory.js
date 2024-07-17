edb0fd5ab0ee2a150b605013427b4f4d82d2144d
alinaincodeland <alinacuznetov@gmail.com>
2024-07-17 10:53:46 +0200

    Add console log comment.

--- factory.js
+++ factory.js
@@ -15,15 +15,15 @@ function findFirstRepeated(gifts) {
 }
 
 const firstRepeatedId = findFirstRepeated(giftIds);
-console.log(firstRepeatedId); // 3
+console.log("Factory challenge", firstRepeatedId); // 3
 // Even though 2 and 3 are repeated
 // 3 appears second time first
 
 const giftIds2 = [1, 2, 3, 4];
 const firstRepeatedId2 = findFirstRepeated(giftIds2);
-console.log(firstRepeatedId2); // -1
+console.log("Factory challenge", firstRepeatedId2); // -1
 // It is -1 since no number is repeated
 
 const giftIds3 = [5, 1, 5, 1];
 const firstRepeatedId3 = findFirstRepeated(giftIds3);
-console.log(firstRepeatedId3); // 5
+console.log("Factory challenge", firstRepeatedId3); // 5
